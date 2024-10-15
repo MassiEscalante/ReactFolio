b530e85754e8a6c0c26f29697f142bd7c699a46d
MassiEscalante <massiel2611@gmail.com>
2024-10-14 17:03:04 -0700

    added navbar animation and fixed text in header

--- vite-project/src/components/Header.jsx
+++ vite-project/src/components/Header.jsx
@@ -6,10 +6,10 @@ function Header() {
   return (
     <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
       <Container>
-        <Navbar.Brand as={Link} to="/">Developer's Portfolio</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/">Massiel's Portfolio</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
+          <Nav className="ms-auto"> {/* ms-auto to move links to the right */}
             <Nav.Link as={Link} to="/">About Me</Nav.Link>
             <Nav.Link as={Link} to="/portfolio">Portfolio</Nav.Link>
             <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
