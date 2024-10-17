1e1a79473d9327cdaf095c1788e2c7bfb90714c5
MassiEscalante <massiel2611@gmail.com>
2024-10-17 13:48:10 -0700

    added animation in projects page and moved grid to the right

--- vite-project/src/views/Portfolio.jsx
+++ vite-project/src/views/Portfolio.jsx
@@ -38,7 +38,21 @@ function Portfolio() {
   return (
     <section className="portfolio">
       <h2>Projects</h2> {/* Clearer title */}
-      <div className="project-grid"> {/* Grid layout for projects */}
+      
+      {/* Lottie Animation on the left side */}
+      <div className="animation-container">
+        <dotlottie-player
+          src="https://lottie.host/b69881fb-3ee7-48c5-9bc9-85c2984e5fbb/9GKnuhBbnT.json"
+          background="transparent"
+          speed="1"
+          style={{ width: "300px", height: "300px" }}
+          loop
+          autoplay
+        ></dotlottie-player>
+      </div>
+
+      {/* Grid layout for projects */}
+      <div className="project-grid">
         {projects.map((project, index) => (
           <Project
             key={index}
